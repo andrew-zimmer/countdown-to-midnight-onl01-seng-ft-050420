83f14b055caabd0b97725f459a69f0954a8b7d7a
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-05 17:03:10 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ def countdown(sec)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(sec)
+  count = sec 
+  while count > 0 
+    puts "#{count} SECOND(S)!"
+    count -= 1 
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+end
